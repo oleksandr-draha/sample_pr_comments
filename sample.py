afa914ef711a6915b8159e140cdb2c30662b5626
Oleksandr Draha <oleksandr.draha@takeoff.com>
2023-02-15 11:11:38 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,22 +1,17 @@
 import re
 import datetime
+import ast
 from collections import OrderedDict
+
 
 def create_racer_abbreviations_dict(file_name):
     """Retrieves {'abbreviation': (name, team)}" format dict from abbreviations.txt"""
-    abbreviations_dict = {}
     with open(file_name, 'r') as fn:
-        for line in fn:
-            matchObj = re.match(r'^(\w+)_([a-zA-Z\s]+)_([a-zA-Z\s]+)$', line)
-            # group(1) is abbreviation, i.e 'SVM'
-            abbreviations_dict[matchObj.group(1)] = (
-                matchObj.group(2),  # name of a pilot
-                matchObj.group(3).rstrip(),  # team
-            )
+        abbreviations_dict = ast.literal_eval(fn.read())
     return abbreviations_dict
 
 
-# {'abbreviation of pilot': ('name of pilot, 'team')}
+# {'abbreviation of pilot': ('name of pilot', 'team')}
 abbr_dict = create_racer_abbreviations_dict(
     'abbreviations.txt')
 
@@ -40,35 +35,43 @@ start_timings = retrieve_timings_from_log('start.log')
 end_timings = retrieve_timings_from_log('end.log')
 
 
-def sorted_individual_results(start_timings_, end_timings_, abbr_dict_, reverse_order=False):
+def sorted_individual_results(start_timings_, end_timings_, reverse_order=False):
     """
     Receives start and end timings and returns an OrderedDict with
     {abbreviations:timedeltas}
     """
     # creating dict with best lap results
-    lap_results = {key: end_timings_[key] - start_timings_.get(key, 0)
+    lap_results = {key: end_timings_.get(key, datetime.datetime.now()) - start_timings_[key]
                    for key in start_timings_.keys()}
-    sorted_results = dict(
+    sorted_results = OrderedDict(
         sorted(lap_results.items(), key=lambda x: x[1], reverse=reverse_order))
     return sorted_results
 
 
 sorted_lap_results = sorted_individual_results(
-    start_timings, end_timings, abbr_dict)
+    start_timings, end_timings)
+
+
+def timedelta_to_str(timedelta_value):
+    hours, remainder = divmod(timedelta_value.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{str(minutes).zfill(2)}:{str(seconds).zfill(2)}:{str(timedelta_value.microseconds).zfill(6)}"
+
 
 # prints result board to a console
-def print_result_board(sorted_lap_results_):
+def print_result_board(sorted_lap_results_, abbr_dict_):
     counter = 1
     for key, value in sorted_lap_results_.items():
-        racer_name = abbr_dict[key][0]
-        team_name = abbr_dict[key][1]
-        best_time = str(value)[2:-3]
+        driver_data = abbr_dict_.get(key, ("Unknown driver", "Unknown team"))
+        racer_name = driver_data[0]
+        team_name = driver_data[1]
+        best_time = timedelta_to_str(value)
         print(("{: <3} {: <18} | {: <30}  | {}".format(
-            str(counter)+'.', racer_name, team_name, best_time)))
+            str(counter) + '.', racer_name, team_name, best_time)))
         if counter == 15:
             print(
                 '----------------------------------------------------------------------')
         counter += 1
 
 
-print_result_board(sorted_lap_results)
+print_result_board(sorted_lap_results, abbr_dict)
